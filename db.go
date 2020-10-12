4137d977b0ecc0d823f2b4d3163241eecef74555
gaoffan <731906989@qq.com>
2020-10-12 13:10:46 +0800

    查看某人题目回答详情

--- db.go
+++ db.go
@@ -9,43 +9,43 @@ import (
 )
 
 type Score struct {
-	Id int
+	Id       int
 	AnswerId int `db:"answerId"`
-	Score int
+	Score    int
 	MarkerId int `db:"markerId"`
 }
 
 type User struct {
-	Id int
-	Name string
+	Id       int
+	Name     string
 	Password string
-	Email string
-	IsAdmin bool `db:"isAdmin"`
+	Email    string
+	IsAdmin  bool      `db:"isAdmin"`
 	CreateAt time.Time `db:"createAt"`
 	UpdateAt time.Time `db:"updateAt"`
 }
 
-type Question struct{
-	Id int
-	Title string
-	Content string
-	Hidden bool
+type Question struct {
+	Id       int
+	Title    string
+	Content  string
+	Hidden   bool
 	CreateAt time.Time `db:"createAt"`
 	UpdateAt time.Time `db:"updateAt"`
 }
 
-type Answer struct{
-	Id int
-	Content string
-	UserId int `db:"userId"`
-	QuestionId int `db:"questionId"`
-	CreateAt time.Time `db:"createAt"`
-	UpdateAt time.Time `db:"updateAt"`
+type Answer struct {
+	Id         int
+	Content    string
+	UserId     int       `db:"userId"`
+	QuestionId int       `db:"questionId"`
+	CreateAt   time.Time `db:"createAt"`
+	UpdateAt   time.Time `db:"updateAt"`
 }
 
 type Status struct {
-	Count int64
-	Done int64
+	Count   int64
+	Done    int64
 	Percent float64
 }
 
@@ -62,96 +62,98 @@ CREATE TABLE IF NOT EXISTS score (
 
 func connectDb() {
 	var err error
-	//db, err = gorm.Open(
-	//	postgres.Open("host=localhost user=root password=root dbname=pg port=5432 sslmode=disable TimeZone=Asia/Shanghai"), &gorm.Config{
-	//		NamingStrategy: schema.NamingStrategy{
-	//			SingularTable: true,
-	//		},
-	//
-	//	})
 	db, err = sqlx.Connect("postgres", "host=localhost user=root password=root dbname=pg port=5432 sslmode=disable TimeZone=Asia/Shanghai")
-	if err != nil{
+	if err != nil {
 		fmt.Println("database connect error!")
 		os.Exit(-1)
 	}
 	db.MustExec(schema)
 }
 
-func queryAllQuestions() *[]Question{
+func queryAllQuestions() *[]Question {
 	var result []Question
 	err := db.Select(&result, "SELECT * FROM question")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &result
 }
-func queryQuestionById(questionId int) *Question{
+func queryQuestionById(questionId int) *Question {
 	var result Question
-	err := db.Get(&result,`SELECT * FROM question WHERE id = $1`,questionId)
-	if err != nil{
+	err := db.Get(&result, `SELECT * FROM question WHERE id = $1`, questionId)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &result
 }
 
-func queryAllAnswer(questionId int) *[]Answer{
+func queryAllAnswer(questionId int) *[]Answer {
 	var result []Answer
-	err := db.Select(&result,`SELECT * FROM answer WHERE "questionId" = $1 ORDER BY id`,questionId)
-	if err != nil{
+	err := db.Select(&result, `SELECT * FROM answer WHERE "questionId" = $1 ORDER BY id`, questionId)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &result
 }
 
-func queryScore(answerId int) *Score{
+func queryScore(answerId int) *Score {
 	var result Score
-	err := db.Get(&result,`SELECT * FROM score WHERE "answerId" = $1`,answerId)
-	if err != nil{
+	err := db.Get(&result, `SELECT * FROM score WHERE "answerId" = $1`, answerId)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &result
 }
 
-func createScore(score *Score){
+func createScore(score *Score) {
 	tx := db.MustBegin()
 	var s Score
-	tx.Get(&s,`SELECT * FROM score WHERE "answerId" = $1`,score.AnswerId)
-	if s.Id == 0{
-		tx.MustExec(`INSERT INTO score ("answerId",score,"markerId") VALUES ($1,$2,$3)`,score.AnswerId,score.Score,score.MarkerId)
-	}else{
-		tx.MustExec(`UPDATE score SET score = $1,"markerId" = $2 WHERE id = $3`,score.Score,score.MarkerId,s.Id)
+	tx.Get(&s, `SELECT * FROM score WHERE "answerId" = $1`, score.AnswerId)
+	if s.Id == 0 {
+		tx.MustExec(`INSERT INTO score ("answerId",score,"markerId") VALUES ($1,$2,$3)`, score.AnswerId, score.Score, score.MarkerId)
+	} else {
+		tx.MustExec(`UPDATE score SET score = $1,"markerId" = $2 WHERE id = $3`, score.Score, score.MarkerId, s.Id)
 	}
 	tx.Commit()
 }
 
-func queryUser(email string) *User{
+func queryUser(email string) *User {
 	var result User
-	err := db.Get(&result,`SELECT * FROM "user" WHERE email = $1`,email)
-	if err != nil{
+	err := db.Get(&result, `SELECT * FROM "user" WHERE email = $1`, email)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &result
 }
 
-func queryUserById(id int) *User{
+func queryUserById(id int) *User {
 	var result User
-	err := db.Get(&result,`SELECT * FROM "user" WHERE id = $1`,id)
-	if err != nil{
+	err := db.Get(&result, `SELECT * FROM "user" WHERE id = $1`, id)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return &result
 }
 
-func queryJudgeProgress(questionId int) Status{
+func queryJudgeProgress(questionId int) Status {
 	var count int64
 	var done int64
-	err := db.Get(&count,`SELECT count(1) FROM answer WHERE "questionId" = $1`,questionId)
-	if err != nil{
+	err := db.Get(&count, `SELECT count(1) FROM answer WHERE "questionId" = $1`, questionId)
+	if err != nil {
 		fmt.Println(err)
 	}
-	err = db.Get(&done,`SELECT count(1) FROM answer RIGHT JOIN score ON score."answerId" = answer.id WHERE "questionId" = $1`,questionId)
-	if err != nil{
+	err = db.Get(&done, `SELECT count(1) FROM answer RIGHT JOIN score ON score."answerId" = answer.id WHERE "questionId" = $1`, questionId)
+	if err != nil {
 		fmt.Println(err)
 	}
-	return Status{count,done,float64(done) / float64(count) * 100}
+	return Status{count, done, float64(done) / float64(count) * 100}
+}
+
+func queryAnswers(userId int) *[]Answer {
+	var result []Answer
+	err := db.Select(&result, `SELECT * FROM answer WHERE "userId" = $1 ORDER BY "questionId"`, userId)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return &result
 }
--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 	r.POST("/answers/:questionId/:num", mark)
 	r.GET("/login", loginPage)
 	r.POST("/login", login)
+	r.GET("/detail", detail)
 	r.Run(":10000")
 }
 
@@ -32,52 +33,53 @@ func render() multitemplate.Renderer {
 	r.AddFromFiles("index", "templates/layout.html", "templates/index.html")
 	r.AddFromFiles("login", "templates/layout.html", "templates/login.html")
 	r.AddFromFiles("answer", "templates/layout.html", "templates/answer.html")
+	r.AddFromFiles("detail", "templates/layout.html", "templates/detail.html")
 	return r
 }
 
-func checkIsLogin(c *gin.Context)bool{
-	cookie,err := c.Cookie("token")
-	if err != nil{
-		c.Redirect(http.StatusFound,"/login")
+func checkIsLogin(c *gin.Context) bool {
+	cookie, err := c.Cookie("token")
+	if err != nil {
+		c.Redirect(http.StatusFound, "/login")
 		return false
 	}
-	id,_ := strconv.Atoi(cookie)
-	_,exist := session[id]
-	if !exist{
-		c.Redirect(http.StatusFound,"/login")
+	id, _ := strconv.Atoi(cookie)
+	_, exist := session[id]
+	if !exist {
+		c.Redirect(http.StatusFound, "/login")
 		return false
 	}
 	return true
 }
 
-func index(c *gin.Context){
-	if !checkIsLogin(c){
+func index(c *gin.Context) {
+	if !checkIsLogin(c) {
 		return
 	}
 	questionId, _ := strconv.Atoi(c.Param("questionId"))
 
 	allQuestions := *queryAllQuestions()
-	progress := make(map[int]Status,0)
-	for _,item := range allQuestions{
+	progress := make(map[int]Status, 0)
+	for _, item := range allQuestions {
 		progress[item.Id] = queryJudgeProgress(item.Id)
 	}
 
 	if questionId != 0 {
 		allAnswers := *queryAllAnswer(questionId)
-		c.HTML(http.StatusOK, "index" , gin.H{
-			"thisQuestion" : queryQuestionById(questionId),
-			"thisProgress" : queryJudgeProgress(questionId),
-			"allAnswers" :      allAnswers,
+		c.HTML(http.StatusOK, "index", gin.H{
+			"thisQuestion": queryQuestionById(questionId),
+			"thisProgress": queryJudgeProgress(questionId),
+			"allAnswers":   allAnswers,
 		})
-	}else{
-		c.HTML(http.StatusOK, "index" , gin.H{
-			"progress" : progress,
+	} else {
+		c.HTML(http.StatusOK, "index", gin.H{
+			"progress": progress,
 		})
 	}
 }
 
-func answers(c *gin.Context){
-	if !checkIsLogin(c){
+func answers(c *gin.Context) {
+	if !checkIsLogin(c) {
 		return
 	}
 	questionId, _ := strconv.Atoi(c.Param("questionId"))
@@ -87,22 +89,22 @@ func answers(c *gin.Context){
 	marker := queryUserById(score.MarkerId)
 
 	nextId := num
-	if len(answers) - 1 > num{
+	if len(answers)-1 > num {
 		nextId++
 	}
 
-	c.HTML(http.StatusOK, "answer" , gin.H{
-		"thisQuestion" : queryQuestionById(questionId),
-		"answer" :       answers[num],
-		"score" :        score,
-		"marker" :       marker,
-		"nextId" :       nextId,
-		"progress" :     queryJudgeProgress(questionId),
+	c.HTML(http.StatusOK, "answer", gin.H{
+		"thisQuestion": queryQuestionById(questionId),
+		"answer":       answers[num],
+		"score":        score,
+		"marker":       marker,
+		"nextId":       nextId,
+		"progress":     queryJudgeProgress(questionId),
 	})
 }
 
-func mark(c *gin.Context){
-	if !checkIsLogin(c){
+func mark(c *gin.Context) {
+	if !checkIsLogin(c) {
 		return
 	}
 	questionId, _ := strconv.Atoi(c.Param("questionId"))
@@ -116,24 +118,24 @@ func mark(c *gin.Context){
 	if err != nil {
 		success = false
 	} else {
-		user,_ := c.Cookie("token")
-		userId,_ := strconv.Atoi(user)
+		user, _ := c.Cookie("token")
+		userId, _ := strconv.Atoi(user)
 		score.AnswerId = answers[num].Id
 		score.MarkerId = session[userId]
 		createScore(&score)
 	}
 
-	if success && len(answers) - 1 > num{
+	if success && len(answers)-1 > num {
 		num++
 	}
-	c.Redirect(http.StatusFound, "/answers/" + c.Param("questionId") + "/" + strconv.Itoa(num))
+	c.Redirect(http.StatusFound, "/answers/"+c.Param("questionId")+"/"+strconv.Itoa(num))
 }
 
-func loginPage(c *gin.Context){
+func loginPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "login", gin.H{})
 }
 
-func login(c *gin.Context){
+func login(c *gin.Context) {
 	email := c.PostForm("email")
 	password := c.PostForm("password")
 	hash := md5.New()
@@ -141,18 +143,30 @@ func login(c *gin.Context){
 	password = hex.EncodeToString(hash.Sum(nil))
 
 	user := queryUser(email)
-	if user.Id == 0 || !user.IsAdmin{
+	if user.Id == 0 || !user.IsAdmin {
 		c.HTML(http.StatusOK, "login", gin.H{
-			"error" : "不是管理员账号",
+			"error": "不是管理员账号",
 		})
-	}else if user.Password != password{
+	} else if user.Password != password {
 		c.HTML(http.StatusOK, "login", gin.H{
-			"error" : "密码错误",
+			"error": "密码错误",
 		})
-	}else {
+	} else {
 		newSession := rand.Int()
 		session[newSession] = user.Id
-		c.SetCookie("token",strconv.Itoa(newSession),0,"/","",false,true)
-		c.Redirect(http.StatusFound,"/")
+		c.SetCookie("token", strconv.Itoa(newSession), 0, "/", "", false, true)
+		c.Redirect(http.StatusFound, "/")
 	}
+}
+
+func detail(c *gin.Context) {
+	if !checkIsLogin(c) {
+		return
+	}
+	email := c.Query("email")
+	user := queryUser(email)
+	c.HTML(http.StatusOK, "detail", gin.H{
+		"user":    user,
+		"answers": queryAnswers(user.Id),
+	})
 }
